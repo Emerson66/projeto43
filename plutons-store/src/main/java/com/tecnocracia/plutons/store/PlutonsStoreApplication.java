3b2f350c577939d7e1d179fad4c36ab9c074e3ee
emerson <emerson@emerson-HP-14-Notebook-PC>
2020-08-25 16:30:28 -0400

    projeto43 comit

---
+++ plutons-store/src/main/java/com/tecnocracia/plutons/store/PlutonsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tecnocracia.plutons.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlutonsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlutonsStoreApplication.class, args);
+	}
+
+}
---
+++ plutons-store/src/test/java/com/tecnocracia/plutons/store/PlutonsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnocracia.plutons.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlutonsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
